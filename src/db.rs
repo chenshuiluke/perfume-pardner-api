05cd20a191224743dae8fd9abe9fae4e556b46f3
Luke Chen Shui <chenshuiluke@gmail.com>
2024-05-05 17:59:40 -0500

    DB connection pool is now a singleton

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,14 @@
+use once_cell::sync::OnceCell;
 use sqlx::postgres::PgPool;
 use sqlx::postgres::PgPoolOptions;
 
-pub async fn connection_builder() -> Result<PgPool, sqlx::Error> {
+pub static DB_POOL: OnceCell<PgPool> = OnceCell::new();
+pub async fn init_db_pool() -> Result<(), sqlx::Error> {
     let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
-    PgPoolOptions::new().max_connections(5).connect(&url).await
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&url)
+        .await?;
+    DB_POOL.set(pool);
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ mod app;
 mod db;
 use actix_web::{get, web, App, HttpServer, Responder, Result as ActixResult};
 use app::fragrantica_data;
-use dotenv::dotenv;
+use dotenvy::dotenv;
 use serde::{Deserialize, Serialize};
 use sqlx::postgres::PgPool;
 
+use once_cell::sync::Lazy;
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct FragranceDbRecord {
     id: i32,
@@ -24,13 +25,13 @@ struct FragranceSearch {
 
 #[get("/fragrances")]
 async fn get_fragrances(search: web::Query<FragranceSearch>) -> ActixResult<impl Responder> {
-    let pool = db::connection_builder().await.unwrap();
+    let pool = db::DB_POOL.get().expect("Could not get db pool");
     let perfume_query = sqlx::query_as!(
         FragranceDbRecord,
         "SELECT * FROM perfume WHERE LOWER(name) LIKE $1 OR LOWER(designer) LIKE $1",
         search.query
     )
-    .fetch_all(&pool)
+    .fetch_all(pool)
     .await;
     let perfumes = match perfume_query {
         Ok(perfumes) => perfumes,
@@ -49,9 +50,10 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let pool = db::connection_builder().await.unwrap();
+    db::init_db_pool().await.unwrap();
+    let pool = db::DB_POOL.get().expect("Could not get db pool");
     let perfume_query = sqlx::query!("SELECT name FROM perfume")
-        .fetch_all(&pool)
+        .fetch_all(pool)
         .await;
     let perfumes = match perfume_query {
         Ok(perfumes) => perfumes,
@@ -60,7 +62,7 @@ async fn main() -> std::io::Result<()> {
             vec![]
         }
     };
-    load_fragrances_from_json_to_db(pool).await;
+    load_fragrances_from_json_to_db(pool.clone()).await;
     HttpServer::new(|| App::new().service(get_fragrances).service(greet))
         .bind(("0.0.0.0", 8080))?
         .run()
@@ -70,9 +72,9 @@ async fn main() -> std::io::Result<()> {
 async fn load_fragrances_from_json_to_db(pool: PgPool) {
     match fragrantica_data::read_from_file("./output.json") {
         Ok(fragrances) => {
-            sqlx::query!("TRUNCATE perfume").execute(&pool).await;
+            let _ = sqlx::query!("TRUNCATE perfume").execute(&pool).await;
             for fragrance in fragrances.results[0].hits.iter() {
-                sqlx::query!("INSERT INTO perfume (name, designer, release_year, thumbnail, fragrantica_id, fragrantica_url) VALUES($1, $2, $3, $4, $5, $6)",
+                let _ = sqlx::query!("INSERT INTO perfume (name, designer, release_year, thumbnail, fragrantica_id, fragrantica_url) VALUES($1, $2, $3, $4, $5, $6)",
                 fragrance.naslov,
                 fragrance.dizajner,
                 fragrance.godina,
